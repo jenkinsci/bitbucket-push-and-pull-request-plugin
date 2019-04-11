9dad2d8031e086d1b56a06cc9e1a8b9bca7f39cd
Christian Del Monte <c.monte@qipu.de>
2019-04-11 11:53:24 +0200

    remove unused imports

--- src/test/java/io/jenkins/plugins/bitbucketpushandpullrequest/BitBucketPPRJobProbeTest.java
+++ src/test/java/io/jenkins/plugins/bitbucketpushandpullrequest/BitBucketPPRJobProbeTest.java
@@ -1,7 +1,28 @@
+/*******************************************************************************
+ * The MIT License
+ * 
+ * Copyright (C) 2019, CloudBees, Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+
 package io.jenkins.plugins.bitbucketpushandpullrequest;
 
 import static org.junit.Assert.assertEquals;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,7 +40,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -29,13 +49,11 @@ import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonReader;
 
-import hudson.model.Job;
 import hudson.security.ACL;
 import io.jenkins.plugins.bitbucketpushandpullrequest.action.BitBucketPPRAction;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.BitBucketPPRPayload;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.cloud.BitBucketPPRNewPayload;
 import jenkins.model.Jenkins;
-import jenkins.model.ParameterizedJobMixIn.ParameterizedJob;
 
 
 @RunWith(PowerMockRunner.class)
@@ -59,14 +77,9 @@ public class BitBucketPPRJobProbeTest {
   @Test
   public void testTriggerMatchingJobs() {
     PowerMockito.mockStatic(Jenkins.class);
-
-
     PowerMockito.verifyStatic(Jenkins.class);
-
-
     jobProbe.triggerMatchingJobs(null, null);
     Jenkins.get();
-
   }
 
   @Test
@@ -115,5 +128,4 @@ public class BitBucketPPRJobProbeTest {
 
     return payload;
   }
-
 }
