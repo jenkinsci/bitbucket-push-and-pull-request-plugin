6db518b27d8a9c8660e58b477c4bf09b0c60f9f3
Christian Del Monte <delmonte@silpion.de>
2020-01-27 19:36:23 +0100

    refactor excepitons

--- src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/exception/JobNotStartedException.java
+++ src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/exception/JobNotStartedException.java
@@ -1,10 +1,9 @@
 package io.jenkins.plugins.bitbucketpushandpullrequest.exception;
 
 public class JobNotStartedException extends Exception {
-
+    private static final long serialVersionUID = 1L;
+    
     public JobNotStartedException(String msg) {
         super(msg);
     }
-
-    private static final long serialVersionUID = 1L;
 }
