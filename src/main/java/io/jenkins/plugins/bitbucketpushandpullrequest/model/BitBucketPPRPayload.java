bf979d2225b0d21674acd42eb16284b2a99a8b9c
Patrick Mac Gregor <patrick.macgregor@basilicom.de>
2020-03-10 18:04:25 +0100

    remove old payload model

--- src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/model/BitBucketPPRPayload.java
+++ src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/model/BitBucketPPRPayload.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * The MIT License
- * 
+ *
  * Copyright (C) 2018, CloudBees, Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -28,7 +28,6 @@ import io.jenkins.plugins.bitbucketpushandpullrequest.model.cloud.BitBucketPPRAp
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.cloud.BitBucketPPRPullRequest;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.cloud.BitBucketPPRPush;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.cloud.BitBucketPPRRepository;
-import io.jenkins.plugins.bitbucketpushandpullrequest.model.old.BitBucketPPROldRepository;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.server.BitBucketPPRServerActor;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.server.BitBucketPPRServerChange;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.server.BitBucketPPRServerPullRequest;
@@ -53,10 +52,6 @@ public interface BitBucketPPRPayload extends Serializable {
   }
 
   default BitBucketPPRApproval getApproval() {
-    return null;
-  }
-
-  default BitBucketPPROldRepository getOldRepository() {
     return null;
   }
 
--- src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/model/BitBucketPPRPayloadFactory.java
+++ src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/model/BitBucketPPRPayloadFactory.java
@@ -1,24 +1,23 @@
 /*******************************************************************************
  * The MIT License
- * 
+ *
  * Copyright (C) 2018, CloudBees, Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
-
 
 package io.jenkins.plugins.bitbucketpushandpullrequest.model;
 
@@ -28,13 +27,16 @@ import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRCo
 import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.REPOSITORY_POST;
 import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.REPOSITORY_PUSH;
 import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.REPOSITORY_SERVER_PUSH;
+
+import java.util.logging.Logger;
+
 import javax.naming.OperationNotSupportedException;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.cloud.BitBucketPPRNewPayload;
-import io.jenkins.plugins.bitbucketpushandpullrequest.model.old.BitBucketPPROldPost;
 import io.jenkins.plugins.bitbucketpushandpullrequest.model.server.BitBucketPPRServerPayload;
 
-
 public class BitBucketPPRPayloadFactory {
+
+  static final Logger LOGGER = Logger.getLogger(BitBucketPPRPayloadFactory.class.getName());
 
   private BitBucketPPRPayloadFactory() {}
 
@@ -46,7 +48,7 @@ public class BitBucketPPRPayloadFactory {
       if (REPOSITORY_PUSH.equalsIgnoreCase(bitbucketEvent.getAction())) {
         payload = new BitBucketPPRNewPayload();
       } else if (REPOSITORY_POST.equalsIgnoreCase(bitbucketEvent.getAction())) {
-        payload = new BitBucketPPROldPost();
+        LOGGER.warning("Got unexpected old post event, ignored!");
       } else if (REPOSITORY_SERVER_PUSH.equalsIgnoreCase(bitbucketEvent.getAction())) {
         payload = new BitBucketPPRServerPayload();
       }
