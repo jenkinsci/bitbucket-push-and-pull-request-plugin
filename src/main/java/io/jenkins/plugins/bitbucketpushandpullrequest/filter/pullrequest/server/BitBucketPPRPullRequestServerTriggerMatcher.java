84eef80ee8e10f27dad23f4b3b20f4b63dacb87e
Patrick Mac Gregor <patrick.macgregor@basilicom.de>
2020-04-07 15:39:26 +0200

    wip

--- src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/filter/pullrequest/server/BitBucketPPRPullRequestServerTriggerMatcher.java
+++ src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/filter/pullrequest/server/BitBucketPPRPullRequestServerTriggerMatcher.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * The MIT License
- * 
- * Copyright (C) 2019, CloudBees, Inc.
- * 
+ *
+ * Copyright (C) 2020, CloudBees, Inc.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -26,6 +26,8 @@ import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRCo
 import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.PULL_REQUEST_SERVER_CREATED;
 import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.PULL_REQUEST_SERVER_MERGED;
 import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.PULL_REQUEST_SERVER_UPDATED;
+import static io.jenkins.plugins.bitbucketpushandpullrequest.util.BitBucketPPRConstsUtils.PULL_REQUEST_SERVER_SOURCE_UPDATED;
+
 
 import io.jenkins.plugins.bitbucketpushandpullrequest.filter.BitBucketPPREventTriggerMatcher;
 import io.jenkins.plugins.bitbucketpushandpullrequest.filter.BitBucketPPRTriggerFilter;
@@ -43,6 +45,9 @@ public class BitBucketPPRPullRequestServerTriggerMatcher
                 || (PULL_REQUEST_SERVER_UPDATED.equalsIgnoreCase(bitbucketEvent.getAction())
                         && triggerFilter
                                 .getActionFilter() instanceof BitBucketPPRPullRequestServerUpdatedActionFilter)
+                || (PULL_REQUEST_SERVER_SOURCE_UPDATED.equalsIgnoreCase(bitbucketEvent.getAction())
+                        && triggerFilter
+                                .getActionFilter() instanceof BitBucketPPRPullRequestServerSourceUpdatedActionFilter)
                 || (PULL_REQUEST_SERVER_CREATED.equalsIgnoreCase(bitbucketEvent.getAction())
                         && triggerFilter
                                 .getActionFilter() instanceof BitBucketPPRPullRequestServerCreatedActionFilter)
@@ -51,3 +56,5 @@ public class BitBucketPPRPullRequestServerTriggerMatcher
                                 .getActionFilter() instanceof BitBucketPPRPullRequestServerMergedActionFilter);
     }
 }
+
+
