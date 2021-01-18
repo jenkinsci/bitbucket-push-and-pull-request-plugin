f21c5de14c9c35fc7b757856ac63fdf5f1b50d93
Ramkumar Krishnamoorthy <rkrishnamoorthy@seamlesscms.com>
2021-01-19 01:04:22 +1100

    Add global configuration to control bitbucket notification

--- src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/observer/BitBucketPPRHandlerTemplate.java
+++ src/main/java/io/jenkins/plugins/bitbucketpushandpullrequest/observer/BitBucketPPRHandlerTemplate.java
@@ -1,20 +1,24 @@
 package io.jenkins.plugins.bitbucketpushandpullrequest.observer;
 
+import io.jenkins.plugins.bitbucketpushandpullrequest.configuration.BitBucketPPRNotifyConfiguration;
 import io.jenkins.plugins.bitbucketpushandpullrequest.event.BitBucketPPREventType;
 
 public abstract class BitBucketPPRHandlerTemplate {
 
   public void run(BitBucketPPREventType eventType) throws Exception {
-    switch (eventType) {
-      case BUILD_STARTED:
-        setBuildStatusInProgress();
-        break;
-      case BUILD_FINISHED:
-        setBuildStatusOnFinished();
-        setApproved();
-        break;
-      default:
-        throw new Exception();
+    BitBucketPPRNotifyConfiguration config = BitBucketPPRNotifyConfiguration.get();
+    if(config.isNotifyBitBucket()) {
+      switch (eventType) {
+        case BUILD_STARTED:
+          setBuildStatusInProgress();
+          break;
+        case BUILD_FINISHED:
+          setBuildStatusOnFinished();
+          setApproved();
+          break;
+        default:
+          throw new Exception();
+      }
     }
   }
 
